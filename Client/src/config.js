adedb9f51295ef1aac05cd663a8daea7b80254b3
avmaurya07 <avmaurya07@gmail.com>
2024-10-13 20:29:28 +0530

    uploading code

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,4 +1,4 @@
 // const BACKEND_HOST = "http://localhost:5000";
-const BACKEND_HOST = "https://event-management-9zzr.onrender.com/";
+const BACKEND_HOST = "https://event-management-9zzr.onrender.com";
 
 export default BACKEND_HOST;
